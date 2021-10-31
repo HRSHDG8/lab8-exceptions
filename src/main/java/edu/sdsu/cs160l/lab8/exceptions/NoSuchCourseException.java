7bccafc6528b23df2f5961701d537d4f0d69b089
Harsh Maheshwari <hrshdg8@gmail.com>
2021-10-31 10:05:32 -0700

    main

--- src/main/java/edu/sdsu/cs160l/lab8/exceptions/NoSuchCourseException.java
+++ src/main/java/edu/sdsu/cs160l/lab8/exceptions/NoSuchCourseException.java
@@ -2,6 +2,6 @@ package edu.sdsu.cs160l.lab8.exceptions;
 
 import java.util.NoSuchElementException;
 
-//TODO implement this exception 1point as per the bluePrint
+//TODO implement this exception 1point as per the bluePrint in ClassFullException
 public class NoSuchCourseException extends NoSuchElementException {
 }
--- src/main/java/edu/sdsu/cs160l/lab8/exceptions/StudentAlreadyEnrolledException.java
+++ src/main/java/edu/sdsu/cs160l/lab8/exceptions/StudentAlreadyEnrolledException.java
@@ -1,5 +1,5 @@
 package edu.sdsu.cs160l.lab8.exceptions;
 
-//TODO implement this exception 1point as per the bluePrint
+//TODO implement this exception 1point as per the bluePrint in ClassFullException
 public class StudentAlreadyEnrolledException extends Exception{
 }
